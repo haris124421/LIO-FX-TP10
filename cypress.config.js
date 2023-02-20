e2373dd3fbf80e6ba07e5005e9e1ec202a3467fc
haris <haris.fastian12@gmail.com>
2023-02-20 17:12:43 +0500

    added Junit reported in config

--- cypress.config.js
+++ cypress.config.js
@@ -22,5 +22,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/my-test-output.xml'
+  },
   },
 });
