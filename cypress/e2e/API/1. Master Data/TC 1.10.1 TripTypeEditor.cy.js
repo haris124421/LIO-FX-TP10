e4065142466689955dbc453422867ee97466f84d
haris <haris.fastian12@gmail.com>
2023-02-23 16:32:48 +0500

    added PUT request for TripTypeEditor

--- cypress/e2e/API/1. Master Data/TC 1.10.1 TripTypeEditor.cy.js
+++ cypress/e2e/API/1. Master Data/TC 1.10.1 TripTypeEditor.cy.js
@@ -3,9 +3,9 @@ import businessArea_data from "../../../fixtures/api_data/businessArea.json"
 import commonFucntions from "../../../helpers/commonFunctions";
 import jwtToken from "../../../fixtures/api_data/loginToken.json"
 
-describe('Allowance', ()=> {
+describe('Trip Type', ()=> {
 
-    it('TC 1.9.1 it should add Allowance successfully', () => {
+    it('TC 1.10.1 it should add Trip Type successfully', () => {
         cy.request({
             method : 'PUT',
             url: Cypress.env('BaseURLApi')+'/LIOWebAPI/api/AllowanceEditor',
@@ -21,21 +21,23 @@ describe('Allowance', ()=> {
                 "isFreezeSchedule":false,
                 "OrderBy":null,
                 "OrderType":null,
-                "AllowanceDetail":{
+                "TripTypeDetail":{
                     "Id":"",
                     "Name":commonFucntions.generate_random_string(8),
-                    "AllowanceConcurrencyValue":"",
+                    "Number1":commonFucntions.getRandomArbitrary(100,1000),
+                    "TripTypeConcurrencyValue":"",
                     "Abbreviation":commonFucntions.generate_random_string(5),
-                    "Type":commonFucntions.getRandomArbitrary(1,4),
                     "UnitId":company_data.UpdatedUnitId,
                     "BusinessAreaId":businessArea_data.UpdatedBusinessAreaId,
-                    "Color":"#b02424",
+                    "Annotation":"",
+                    "Color":"#000000",
+                    "BackgroundColor":"transparent",
                     "RevisionId":""
                  }
              }
         }).then( (response) =>{
             expect(`Response.status = ${response.body.OperationStatus.HttpStatusCode}`).to.eq('Response.status = 200')
-            cy.writeFile('cypress/fixtures/api_data/allowance.json',response.body)
+            cy.writeFile('cypress/fixtures/api_data/tripType.json',response.body)
         })
     }) 
 })
