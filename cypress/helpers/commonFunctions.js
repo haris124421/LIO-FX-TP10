8af7f4e409c95e689a924b3e47aead4d0896d8b9
haris <haris.fastian12@gmail.com>
2023-02-28 18:03:28 +0500

    Trigger for 5.9.0.12

--- cypress/helpers/commonFunctions.js
+++ cypress/helpers/commonFunctions.js
@@ -1,7 +1,7 @@
 class commonFucntions{
 
     static generate_random_string(string_length) {
-        let random_string = 'Automation 5.9.0.11'+' ';
+        let random_string = 'Automation 5.9.0.12'+' ';
         let random_ascii;
         for(let i = 0; i < string_length; i++) {
             random_ascii = Math.floor((Math.random() * 25) + 97);
